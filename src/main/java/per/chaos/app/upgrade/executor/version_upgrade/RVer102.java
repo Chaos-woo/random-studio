0aa925a9b2ca540a14ae48a4109ab330c6ab8b39
Chaos.woo <785808933@qq.com>
2023-07-23 19:02:27 +0800

    refactor(*): 优化数据版本升级代码结构

--- src/main/java/per/chaos/app/upgrade/executor/version_upgrade/RVer102.java
+++ src/main/java/per/chaos/app/upgrade/executor/version_upgrade/RVer102.java
@@ -9,17 +9,26 @@ import java.sql.SQLException;
 
 @Slf4j
 public class RVer102 {
+    /**
+     * 固定字段,升级基础版本
+     */
+    public static final String fromVersion = "1.0.0";
+    /**
+     * 固定字段,升级最终版本
+     */
+    public static final String toVersion = "1.0.2";
+
     public static void perform() {
         try {
             performCreateTableCtFileRefer();
         } catch (Exception e) {
-            log.warn("执行RVer102-SQL失败");
+            log.warn(String.format("执行 %s to %s 升级失败", fromVersion, toVersion));
+            throw new RuntimeException(e);
         }
     }
 
     private static void performCreateTableCtFileRefer() {
-        @SuppressWarnings("all")
-        final String sql = "CREATE TABLE IF NOT EXISTS \"ct_file_refer\"(" +
+        @SuppressWarnings("all") final String sql = "CREATE TABLE IF NOT EXISTS \"ct_file_refer\"(" +
                 "\"id\" integer NOT NULL," +
                 "\"absolute_path\" text NOT NULL," +
                 "\"path_hash\" text NOT NULL," +
--- src/main/java/per/chaos/infrastructure/utils/formmater/AppFormatter.java
+++ src/main/java/per/chaos/infrastructure/utils/formmater/AppFormatter.java
@@ -4,6 +4,9 @@ import per.chaos.app.context.AppContext;
 import per.chaos.app.models.entry.Project;
 
 public class AppFormatter {
+    /**
+     * 获取应用版本字符串
+     */
     public static String getAppVersion() {
         final Project project = AppContext.instance().getProjectContext().getProject();
         String format = "R.Version.%s";
